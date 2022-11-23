132485075a5e11583956923a1c593edf50d4d54c
JustinDavis-Dev <justin.davis.dev@gmail.com>
2022-11-23 11:30:11 -0700

    testing pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  
   root 'home#index'
+  namespace :api do
+    resources :tasks
+  end
   devise_for :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_023923) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_043934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.boolean "completed"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
