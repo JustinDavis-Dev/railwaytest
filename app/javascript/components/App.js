144dba2d630d0b9bd336807be96e96e9f062b0c0
JustinDavis-Dev <justin.davis.dev@gmail.com>
2022-11-22 19:35:14 -0700

    Set up react

---
+++ app/javascript/components/App.js
@@ -0,0 +1,20 @@
+// import React from "react"
+// import PropTypes from "prop-types"
+// class App extends React.Component {
+//   render () {
+//     return (
+//       <React.Fragment>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// export default App
+
+import React from "react";
+
+function App() {
+	return <h1>App</h1>;
+}
+
+export default App;
